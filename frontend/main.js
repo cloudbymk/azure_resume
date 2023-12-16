65fe1bc207197a2ae56701fcce734772665e8575
cloudbymk <mrudulkulkarni@hotmail.com>
2023-12-16 16:11:22 -0500

    function deployed

--- frontend/main.js
+++ frontend/main.js
@@ -3,14 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) =>
     getVisitCount();
 })
 
-
+const functionApiUrl = "https://getresumecountermk.azurewebsites.net/api/GetAndUpdateCounter?code=df8vkRB3wfduSpNESfXxKuM3RsZkRXMOwrNZBgMh41UzAzFunEzN7w==";
 const functionApi = 'http://localhost:7071/api/GetAndUpdateCounter';
 
 const getVisitCount = () =>
 {
 
         let count = 10;
-        fetch(functionApi).then(Response =>
+        fetch(functionApiUrl).then(Response =>
             {
                 return Response.json()
 
